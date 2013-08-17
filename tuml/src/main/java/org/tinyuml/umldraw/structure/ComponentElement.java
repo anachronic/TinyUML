9e4105fbbdab4f974bda542cf65cb0dcdbcfc804
Nicolás Salas <nikosalas@gmail.com>
2013-08-17 03:11:08 -0400

    Bugfix #2: Completo. Sólo se hacen relaciones que UML permite.
    Cambiado ComponentElement.java: se implementa método acceptsConnection
    para ver que las relaciones son correctas y sólo elementos del mismo
    tipo se relacionan entre sí).
    
    Esto termina con la Solicitud A.

--- tuml/src/main/java/org/tinyuml/umldraw/structure/ComponentElement.java
+++ tuml/src/main/java/org/tinyuml/umldraw/structure/ComponentElement.java
@@ -21,6 +21,7 @@ package org.tinyuml.umldraw.structure;
 
 import java.awt.Color;
 import java.awt.geom.Dimension2D;
+
 import org.tinyuml.draw.AbstractCompositeNode;
 import org.tinyuml.draw.Compartment;
 import org.tinyuml.draw.DrawingContext.FontType;
@@ -224,7 +225,19 @@ implements LabelSource, UmlNode {
    */
   public boolean acceptsConnection(RelationType associationType,
     RelationEndType as, UmlNode with) {
-    return true;
+	// Solicitud A: Cuando se hace la verificación de si SOURCE es válido
+	// se entrega null. Arreglaremos esta cosa aceptando la conexión si
+	// la RelationEndType (as) es SOURCE
+	if(as == RelationEndType.SOURCE) return true;
+	
+	// Solicitud A: La herencia sólo se puede dar entre clases
+	if(associationType == RelationType.INHERITANCE)
+	  return false;
+	  
+	// Solicitud A: Componentes se pueden relacionar entre sí
+	if(with instanceof ComponentElement) return true;
+	
+    return false;
   }
 
   /**

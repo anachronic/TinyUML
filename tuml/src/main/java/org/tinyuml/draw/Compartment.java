826d48c7e6ce52550d5c25373c4bcf9251d25d64
Nicolás Salas <nikosalas@gmail.com>
2013-08-18 07:16:52 -0400

    Issue #6: Solucionado problema de shallow copies.
    Al copiar una clase en otro diagrama sucedía que al modificar cualquiera
    de las clases. Se modifica el archivo Compartment.java.
    
    Específicamente se modifica el método clone() que trataba de acceder a
    variables de instancia privadas. Además, se crea el método setLabels,
    para setear los labels del compartimiento.

--- tuml/src/main/java/org/tinyuml/draw/Compartment.java
+++ tuml/src/main/java/org/tinyuml/draw/Compartment.java
@@ -65,13 +65,13 @@ implements CompositeNode {
   @Override
   public Object clone() {
     Compartment cloned = (Compartment) super.clone();
-    cloned.labels = new ArrayList<Label>();
+    cloned.setLabels(new ArrayList<Label>());
     for (Label label : labels) {
       Label clonedLabel = (Label) label.clone();
       // Note that the source might have changed for the clone
       clonedLabel.setSource(label.getSource());
       clonedLabel.setParent(cloned);
-      cloned.labels.add(clonedLabel);
+      cloned.addLabel(clonedLabel);
     }
     return cloned;
   }
@@ -115,6 +115,14 @@ implements CompositeNode {
   public void addLabel(Label label) {
     labels.add(label);
     label.setParent(this);
+  }
+  
+  /**
+   * Set this object's labels to labels..
+   * @param labels the list of labels.
+   */
+  public void setLabels(List<Label> labels) {
+    this.labels = labels;
   }
 
   /**
